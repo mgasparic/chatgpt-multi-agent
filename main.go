fc84545826a4669629619d48842e293a821fc4f6
sirOliver <marko.gasparic89@gmail.com>
2023-05-06 13:19:03 +0200

    another version, which executes requests, by Bing ChatGPT4

--- main.go
+++ main.go
@@ -1,89 +1,140 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
-	"time"
-
-	chatgpt "github.com/chatgp/chatgpt-go"
+	"os"
+	"strings"
 )
 
-const (
-	PromptEngineer = iota
-	ChatGPTAgent
-	DomainExpert
-)
+type Agent struct {
+	Name           string
+	APIKey         string
+	InitialMessage string
+	ConversationID string
+}
 
-func main() {
-	token := `copy-from-cookies`
-	cfValue := "copy-from-cookies"
-	puid := "copy-from-cookies"
-	cookies := []*http.Cookie{
-		{Name: "__Secure-next-auth.session-token", Value: token},
-		{Name: "cf_clearance", Value: cfValue},
-		{Name: "_puid", Value: puid},
+func NewAgent(name string, apiKey string, initialMessage string, conversationID string) *Agent {
+	return &Agent{
+		Name:           name,
+		APIKey:         apiKey,
+		InitialMessage: initialMessage,
+		ConversationID: conversationID,
+	}
+}
+
+func (a *Agent) GetChatText(prompt string) (string, error) {
+	url := "https://api.openai.com/v1/engines/gpt-3.5-turbo/completions"
+
+	payload := map[string]interface{}{
+		"prompt":          prompt,
+		"conversation_id": a.ConversationID,
+	}
+	payloadBytes, err := json.Marshal(payload)
+	if err != nil {
+		return "", err
 	}
 
-	// create three separate instances of the ChatGPT client
-	promptEngineer := chatgpt.NewClient(
-		chatgpt.WithDebug(true),
-		chatgpt.WithTimeout(60*time.Second),
-		chatgpt.WithCookies(cookies),
-	)
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(payloadBytes))
+	if err != nil {
+		return "", err
+	}
 
-	chatGPTAgent := chatgpt.NewClient(
-		chatgpt.WithDebug(true),
-		chatgpt.WithTimeout(60*time.Second),
-		chatgpt.WithCookies(cookies),
-	)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", a.APIKey))
 
-	domainExpert := chatgpt.NewClient(
-		chatgpt.WithDebug(true),
-		chatgpt.WithTimeout(60*time.Second),
-		chatgpt.WithCookies(cookies),
-	)
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
 
-	// define a simple state machine to keep track of the conversation
-	speaker := PromptEngineer
-	listener := ChatGPTAgent
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
 
-	// define a set of rules or conditions that determine when and how the agents communicate with each other
-	for {
-		var message string
-		var err error
+	var result map[string]interface{}
+	err = json.Unmarshal(body, &result)
+	if err != nil {
+		return "", err
+	}
 
-		switch speaker {
-		case PromptEngineer:
-			fmt.Println("Prompt engineer is speaking")
-
-			// decide whether to switch to communicating with the domain expert
-			if someConditionIsTrue() {
-				fmt.Println("Prompt engineer is switching to communicating with the domain expert")
-				listener = DomainExpert
-				message = "switch to domain expert"
-			} else {
-				fmt.Println("Prompt engineer is communicating with the ChatGPT agent")
-				listener = ChatGPTAgent
-				message = "Hello, ChatGPT agent!"
+	if choices, ok := result["choices"].([]interface{}); ok && len(choices) > 0 {
+		if choice, ok := choices[0].(map[string]interface{}); ok {
+			if text, ok := choice["text"].(string); ok {
+				return text, nil
 			}
+		}
+	}
 
-			_, err = promptEngineer.GetChatText(message)
-		case ChatGPTAgent:
-			fmt.Println("ChatGPT agent is speaking")
-			message = "Hello, prompt engineer!"
-			_, err = chatGPTAgent.GetChatText(message)
-		case DomainExpert:
-			fmt.Println("Domain expert is speaking")
-			message = "Hello, prompt engineer!"
-			_, err = domainExpert.GetChatText(message)
+	return "", fmt.Errorf("failed to get chat text")
+}
+
+func haveConversation(agents []*Agent) {
+	speakerIndex := 0
+	listenerIndex := 1
+
+	for {
+		speaker := agents[speakerIndex]
+		listener := agents[listenerIndex]
+
+		fmt.Printf("%s is speaking\n", speaker.Name)
+
+		message := fmt.Sprintf("Hello, %s!", listener.Name)
+
+		if speaker.InitialMessage != "" {
+			message = speaker.InitialMessage
+			speaker.InitialMessage = ""
 		}
 
+		if message == fmt.Sprintf("I want to speak to %s", listener.Name) {
+			for i, agent := range agents {
+				if agent.Name == listener.Name {
+					listenerIndex = i
+					break
+				}
+			}
+			continue
+		}
+
+		response, err := speaker.GetChatText(message)
 		if err != nil {
 			fmt.Printf("get chat text failed: %v\n", err)
 			break
 		}
 
-		// update the state of the conversation
-		speaker = (speaker + 1) % 3
+		fmt.Printf("%s: %s\n", speaker.Name, response)
+
+		tempIndex := speakerIndex
+		speakerIndex = listenerIndex
+		listenerIndex = tempIndex
 	}
+}
+
+func main() {
+	apiKey := os.Getenv("OPENAI_API_KEY")
+	initialMessages := strings.Split(os.Getenv("INITIAL_MESSAGES"), ",")
+	conversationIDs := strings.Split(os.Getenv("CONVERSATION_IDS"), ",")
+
+	var agents []*Agent
+	for i, name := range strings.Split(os.Getenv("AGENT_NAMES"), ",") {
+		initialMessage := ""
+		if i < len(initialMessages) {
+			initialMessage = initialMessages[i]
+		}
+
+		conversationID := ""
+		if i < len(conversationIDs) {
+			conversationID = conversationIDs[i]
+		}
+
+		agents = append(agents, NewAgent(name, apiKey, initialMessage, conversationID))
+	}
+
+	haveConversation(agents)
 }
